2c5e0f23be19f917d8ee4c774ef031cae4b3f332
obastemur <obastemur@gmail.com>
2015-05-11 04:49:43 +0200

    cleanup

---
+++ io.jxcore.node/src/android/updatelibs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NORMAL_COLOR='\033[0m'
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+GRAY_COLOR='\033[0;37m'
+
+LOG() {
+    COLOR="$1"
+    TEXT="$2"
+    echo -e "${COLOR}$TEXT ${NORMAL_COLOR}"
+}
+
+if [ $# -eq 1 ]
+then
+  cp -R $1/ jxcore-binaries/
+fi
+
+ndk-build
+cp libs/armeabi/* ../../../../platforms/android/libs/armeabi/
+cp libs/armeabi-v7a/* ../../../../platforms/android/libs/armeabi-v7a/
+cp libs/x86/* ../../../../platforms/android/libs/x86/
