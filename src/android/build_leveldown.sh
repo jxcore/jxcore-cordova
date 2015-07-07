1f575019bb9ab015660f0087c7cbab8492c978fa
Oguz Bastemur <obastemur@gmail.com>
2015-07-07 16:26:45 +0200

    remove jxcore base binaries after compiling so

--- src/android/build_leveldown.sh
+++ src/android/build_leveldown.sh
@@ -15,9 +15,14 @@ cp jni/Android.mk.level jni/Android.mk
 
 if [ $# -eq 1 ]
 then
+  rm -rf jxcore-binaries/
+  mkdir jxcore-binaries/
   cp -R $1/ jxcore-binaries/
   rm jxcore-binaries/*_mipsel.a
 	rm jxcore-binaries/*_x64.a	
+else
+  LOG $RED_COLOR "You should provide the path for JXcore Android binaries"
+  exit
 fi
 
 ndk-build
@@ -26,3 +31,5 @@ ndk-build
 $(cp libs/armeabi/* ../../../platforms/android/libs/armeabi/)
 $(cp libs/armeabi-v7a/* ../../../platforms/android/libs/armeabi-v7a/)
 $(cp libs/x86/* ../../../platforms/android/libs/x86/)
+
+rm -rf jxcore-binaries/
