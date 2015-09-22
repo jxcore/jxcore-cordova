26dd8de2415f0e0be010390b6a917e573fcb45e7
Oguz Bastemur <obastemur@gmail.com>
2015-09-22 03:44:04 +0200

    extract release on developer machine

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 jxcore.utils.
-  console.log('JXcore Cordova Plugin is available under io.jxcore.node/ folder.', 'green');
+  console.log('Extracting the binaries', 'yellow');
+
+var AdmZip = require('adm-zip');
+
+var zip = new AdmZip("./io.jxcore.node/bin/ios.zip");
+zip.extractAllTo("./io.jxcore.node/bin/", true);
+
+jxcore.utils.
+  console.log('DONE\n\nJXcore Cordova Plugin is available under io.jxcore.node/ folder.', 'green');
 
 jxcore.utils.
   console.log('type: ', 'yellow');
