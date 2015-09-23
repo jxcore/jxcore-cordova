2a64fa5ea13feaecb780d5aaac0a02f645411c66
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-09-23 13:16:26 +0200

    on windows cmdSync() didn't handle. replaced with execFile()

--- index.js
+++ index.js
@@ -7,28 +7,28 @@ if (process.argv[2] == "EXTRACT") {
   var zip = new AdmZip("./io.jxcore.node/bin/ios.zip");
   zip.extractAllTo("./io.jxcore.node/bin/", true);
 } else {
-  var res = jxcore.utils.cmdSync("cd " + process.cwd() + "; " + process.argv[0] + " io.jxcore.node/index.js EXTRACT");
 
-  if (res.exitCode) {
-    console.error("Something bad happened with the extraction;\n", res.out);
-    process.exit(1);
-  }
+  require("child_process").execFile(process.execPath, ["io.jxcore.node/index.js", "EXTRACT"],
+    {cwd: process.cwd()}, function (error, stdout, stderr) {
 
-  jxcore.utils.
-    console.log('DONE\n\nJXcore Cordova Plugin is available under io.jxcore.node/ folder.', 'green');
+      if (error) {
+        console.error("Something bad happened with the extraction:\n", stdout, "\n", stderr);
+        process.exit(1);
+      }
 
-  jxcore.utils.
-    console.log('type: ', 'yellow');
+      jxcore.utils.
+        console.log('DONE\n\nJXcore Cordova Plugin is available under io.jxcore.node/ folder.', 'green');
 
-  jxcore.utils.
-    console.log('cordova plugins add io.jxcore.node/');
+      jxcore.utils.
+        console.log('type: ', 'yellow');
 
-  jxcore.utils.
-    console.log('to install the plugin', 'yellow');
+      jxcore.utils.
+        console.log('cordova plugins add io.jxcore.node/');
 
-  if (jxcore.utils.OSInfo().isWindows) {
-    jxcore.utils.cmdSync('del io.jxcore.node.jx');
-  } else {
-    jxcore.utils.cmdSync('rm io.jxcore.node.jx');
-  }
+      jxcore.utils.
+        console.log('to install the plugin', 'yellow');
+
+      require('fs').unlinkSync("io.jxcore.node.jx");
+      require('fs').unlinkSync("io.jxcore.node/bin/ios.zip");
+    });
 }
